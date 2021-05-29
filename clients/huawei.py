11fc9d27c00b10ba735f3d18fe8a8d145e450c4e
Shihuan Wang <64689400+wangshihuan@users.noreply.github.com>
2021-05-29 21:24:42 +0800

    Update huawei.py

--- clients/huawei.py
+++ clients/huawei.py
@@ -30,17 +30,21 @@ class HuaWei(BaseHuaWei):
         h = int(utc_dt.astimezone(timezone(timedelta(hours=8))).strftime('%H'))
         await self.start()
 
-
     async def login(self, username, password):
         await self.page.waitForSelector('input[name="userAccount"]')
-        await asyncio.sleep(1)
+        await asyncio.sleep(2)
         await self.page.type('input[name="userAccount"]', username, {'delay': 10})
         await asyncio.sleep(3)
         await self.page.type('.hwid-input-pwd', password, {'delay': 10})
-        await asyncio.sleep(2)
+        await asyncio.sleep(4)
+        items = await self.page.querySelectorAll('.hwid-list-row-active')
+
+        if items and len(items):
+            await items[0].click()
+            await asyncio.sleep(3)
 
         await self.page.click('.normalBtn')
-        await asyncio.sleep(5)
+        await asyncio.sleep(10)
 
     async def iam_login(self, username, password, parent):
         self.parent_user = os.environ.get('PARENT_USER', parent)
